02f879324bd0e7c4c204363fb9dc8de2de2c9206
Stark Riedesel <starkriedesel@gmail.com>
2014-03-26 22:57:18 +0000

    A few changes for serverside stuff to work and spelling correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   # Set this to true to remove the navbar for a page
   @disable_navbar = false
   
-  def after_sign_in_path_for(resource)                                                                                                                      
+  def after_sign_in_path_for(resource)
+    return root_path 
     sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')                                            
     if request.referer == sign_in_url                                                                                                                    
       super                                                                                                                                                 
@@ -20,5 +23,19 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
+  end
+
+  private
+  def authenticate
+    return unless Rails.env.production?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ssig' and password = 'SSIGAdmin1'
+    end
+  end
+    
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit :username, :email, :password, :password_confirmation
+    end
   end
 end
