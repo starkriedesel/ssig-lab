ecdc975967a23fe86e57b94fdfceb74171fc4700
starkriedesel <starkriedesel@gmail.com>
2016-09-17 05:01:12 +0000

    Add missing migration command

--- db/migrate/20150812225544_add_docker_container_id_to_challenge_flags.rb
+++ db/migrate/20150812225544_add_docker_container_id_to_challenge_flags.rb
@@ -1,5 +1,6 @@
 class AddDockerContainerIdToChallengeFlags < ActiveRecord::Migration
   def change
     add_column :challenge_flags, :docker_container_id, :string
+    add_column :challenge_flags, :docker_host_name, :string
   end
 end
